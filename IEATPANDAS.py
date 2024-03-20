cba51c90ded25d5700e36dcc510bf4ef10bf76de
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-20 12:11:32 -0500

    Create IEATPANDAS.py
    I made this in 10 minutes to try and understand pandas.

---
+++ IEATPANDAS.py
@@ -0,0 +1,4 @@
+import pandas as IEATPANDAS
+dict = { "Objects": ["Black hole", "Neutron star", "Star", "Pulsar", "Nicole"], "Location": ["Center of Galaxy", "Space I guess", "Galaxy", "space", "The 4th dimension"], "Purpose": ["Eat stuff", "Make strange matter", "Make planets", "irradiate the universe", "Gain power"]}
+stellar = IEATPANDAS.DataFrame(dict)
+print(stellar)
